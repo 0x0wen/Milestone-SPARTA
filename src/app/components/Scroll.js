afb95510565b6010961eba66bd0b0ab9cff83950
heisenberg <owentobias21@gmail.com>
2023-08-09 11:05:12 +0700

    capek

--- src/app/components/Scroll.js
+++ src/app/components/Scroll.js
@@ -1,21 +1,20 @@
-import { useEffect } from 'react';
+'use client'
+import {useEffect} from 'react'
 
 export default function Scroll(props) {
-  useEffect(() => {
-    function changeNavbar() {
-      if (window.scrollY >= 80) {
-        props.colored();
-      } else {
-        props.transparent();
-      }
-    }
+	useEffect(() => {
+		function changeNavbar() {
+			if (window.scrollY >= 80) {
+				props.colored()
+			} else {
+				props.transparent()
+			}
+		}
+		window.addEventListener('scroll', changeNavbar)
+		return () => {
+			window.removeEventListener('scroll', changeNavbar)
+		}
+	}, [])
 
-    window.addEventListener('scroll', changeNavbar);
-
-    return () => {
-      window.removeEventListener('scroll', changeNavbar);
-    };
-  }, [props.colored, props.transparent]);
-
-  return null;
+	return null
 }
