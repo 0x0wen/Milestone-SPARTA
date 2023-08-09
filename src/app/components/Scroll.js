a6fb8d14f8d203deeec5aaee262f39bb60fceb46
ImanuelGirsang <71023099+ImanuelSG@users.noreply.github.com>
2023-08-09 11:19:19 +0700

    try fix

--- src/app/components/Scroll.js
+++ src/app/components/Scroll.js
@@ -1,18 +1,20 @@
 'use client'
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 
 export default function Scroll(props) {
 	useEffect(() => {
-		function changeNavbar() {
-			if (window.scrollY >= 80) {
-				props.colored()
-			} else {
-				props.transparent()
+		if (typeof window !== 'undefined') {
+			function changeNavbar() {
+				if (window.scrollY >= 80) {
+					props.colored()
+				} else {
+					props.transparent()
+				}
 			}
-		}
-		window.addEventListener('scroll', changeNavbar)
-		return () => {
-			window.removeEventListener('scroll', changeNavbar)
+			window.addEventListener('scroll', changeNavbar)
+			return () => {
+				window.removeEventListener('scroll', changeNavbar)
+			}
 		}
 	}, [])
 
